da0b4db39ccfce8825c6e7fe9aa1f863f7f90990
Michal Stoma <mstoma@sezam.zsz1.edu.pl>
2025-06-20 19:15:27 +0200

    waluty

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ import session from "express-session";
 
 dotenv.config();
 const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const PORT = process.env.PORT || 3000;
 
 const pool = new Pool({
